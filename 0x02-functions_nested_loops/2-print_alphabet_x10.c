9ff5941ccce68bd39e9f99db2e9d145518383808
efiewura2218 <patrickantwiboasiako2218@gmail.com>
2023-12-19 21:18:49 -0800

    islower

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,9 +1,8 @@
 #include "main.h"
 /**
- *print_alpabet_x10 - this is the function
+ *main - print_alpabet_x10 this is the function
  *Return: 0
  */
-/** this is the prototype*/
 void print_alphabet_x10(void)
 {
 char b;
